6debe00072477f18d4fd4c203e784bb5b3cd05af
hbelguenani <haitambelguenani@gmail.com>
2025-04-07 10:31:38 +0200

    data

--- main_file.py
+++ main_file.py
@@ -1,2 +1,21 @@
 import numpy
-import pandas
+import pandas as pd
+
+df = pd.read_csv("machine-learning-groupe-7/Domain1_csv/Subject1-0-1.csv")
+df.columns = ['x', 'y', 'z']  # renomme les colonnes si nécessaire
+
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure(figsize=(10, 6))
+ax = fig.add_subplot(111, projection='3d')
+
+ax.plot(df['x'], df['y'], df['z'], marker='o')
+
+ax.set_title("Trajectoire du geste en 3D")
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z")
+
+plt.show()
