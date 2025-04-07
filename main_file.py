3f2ca62d42a4c82b3f0b3cd8490074381202eacc
hbelguenani <haitambelguenani@gmail.com>
2025-04-07 11:10:22 +0200

    update_good

--- main_file.py
+++ main_file.py
@@ -1,21 +1,27 @@
 import numpy
 import pandas as pd
-
-df = pd.read_csv("data/Domain1_csv/Subject1-0-1.csv")
-df.columns = ['x', 'y', 'z']  # renomme les colonnes si nécessaire
-
-
+import chardet
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+with open("machine-learning-groupe-7/Domain1_csv/Subject7-8-6.csv", 'rb') as f:
+    result = chardet.detect(f.read())
+    print(result)
+    print(f.readline())
+
+df = pd.read_csv("machine-learning-groupe-7/Domain1_csv/Subject7-8-6.csv", encoding=result['encoding'])
+
+df = df.iloc[:, :2]  # on garde seulement les 3 premières colonnes
+df.columns = ['x', 'y']  # renomme les colonnes si nécessaire
+
+
 fig = plt.figure(figsize=(10, 6))
-ax = fig.add_subplot(111, projection='3d')
+ax = fig.add_subplot(111, projection='2d')
 
 ax.plot(df['x'], df['y'], df['z'], marker='o')
 
-ax.set_title("Trajectoire du geste en 3D")
+ax.set_title("Trajectoire du geste en 2D")
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
-ax.set_zlabel("Z")
 
 plt.show()
